f46f90a4c1872d98df269ad2755de96621b6ea9f
MohamedAntar <aboantar852003@gmail.com>
2026-04-10 23:10:07 +0200

    initial structure of redux folder

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Landing from "./components/Landing";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <BrowserRouter>
-      < Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/redux/modules/index.js
@@ -0,0 +1,8 @@
+//index.js
+import { combineReducers } from "redux";
+import users from "./users";
+import alerts from "./alerts";
+import profiles from "./profiles";
+import posts from "./posts";
+
+export default combineReducers({ users, alerts, profiles, posts });
