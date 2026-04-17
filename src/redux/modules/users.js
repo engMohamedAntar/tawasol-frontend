7e8d0616860b1c5f6bb74769ffe1d3d4b880ef2b
MohamedAntar <aboantar852003@gmail.com>
2026-04-17 18:18:21 +0200

    implemented users module in redux folder

--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -3,9 +3,24 @@ import { showAlertMessage } from "./alerts";
 
 const REGISTER_SUCCESS = "users/REGISTER_SUCCESS";
 const REGISTER_FAIL = "users/REGISTER_FAIL";
-// const USER_LOADED = "users/USER_LOADED";
-// const USER_ERROR = "users/USER_ERROR";
+const USER_LOADED = "users/USER_LOADED";
+const USER_ERROR = "users/USER_ERROR";
 
+export const loadUser = () => async (dispatch) => {
+  try {
+    const res = await api.get("/users");
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_ERROR,
+    });
+  }
+};
+
+// Action creator
 export function register(formData) {
   return async function registerThunk(dispatch) {
     try {
@@ -15,6 +30,7 @@ export function register(formData) {
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+      dispatch(loadUser());
     } catch (error) {
       const errors = error.response.data.errors;
       if (errors.length > 0) {
@@ -39,6 +55,13 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
@@ -48,6 +71,15 @@ export default function reducer(state = initialState, action) {
         loading: false,
       };
     case REGISTER_FAIL:
+      localStorage.setItem("token", null);
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+    case USER_ERROR:
       localStorage.setItem("token", null);
       return {
         ...state,
